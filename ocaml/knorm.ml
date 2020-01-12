137592dbcdd2a92d7f18c9d335419bff277caf35
Justus Fasse <justus.fasse@etu.univ-grenoble-alpes.fr>
2020-01-12 22:02:21 +0100

    Add k-normalization for Bool, Sub, Not and Neg.
    Booleans are transformed to 1 (true) and 0 (false)
    
    Sub is copy-pasted from add
    
    Not uses arithmetic 1 - b (where b is either 1 or 0)
    
    Neg subtracts the expression from 0

--- ocaml/knorm.ml
+++ ocaml/knorm.ml
@@ -2,6 +2,7 @@ type t =
   | Unit
   | Int of int
   | Add of Id.t * Id.t
+  | Sub of Id.t * Id.t
   | Let of (Id.t * Type.t) * t * t
   | Var of Id.t
 
@@ -10,6 +11,8 @@ and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
 let rec of_syntax exp_s =
   match exp_s with
   | Syntax.Unit -> Unit
+  | Syntax.Bool true -> Int 1
+  | Syntax.Bool false -> Int 0
   | Syntax.Int i -> Int i
   | Syntax.Add (e1, e2) ->
       let e1_id = Id.genid () in
@@ -18,6 +21,31 @@ let rec of_syntax exp_s =
         ( (e1_id, Type.Int),
           of_syntax e1,
           Let ((e2_id, Type.Int), of_syntax e2, Add (e1_id, e2_id)) )
+  | Syntax.Sub (e1, e2) ->
+      let e1_id = Id.genid () in
+      let e2_id = Id.genid () in
+      Let
+        ( (e1_id, Type.Int),
+          of_syntax e1,
+          Let ((e2_id, Type.Int), of_syntax e2, Sub (e1_id, e2_id)) )
+  | Syntax.Not e ->
+      (* true: 1
+       * false: 0
+       * not true: 1 - 1 -> 0 = true
+       * not false: 1 - 0 -> 1 = false *)
+      let e_id = Id.genid () in
+      let e_1_id = Id.genid () in
+      Let
+        ( (e_id, Type.Int),
+          of_syntax e,
+          Let ((e_1_id, Type.Int), Int 1, Sub (e_1_id, e_id)) )
+  | Syntax.Neg e ->
+      let e_id = Id.genid () in
+      let e_0_id = Id.genid () in
+      Let
+        ( (e_id, Type.Int),
+          of_syntax e,
+          Let ((e_0_id, Type.Int), Int 0, Sub (e_0_id, e_id)) )
   | Syntax.Let ((id, typ), def, body) ->
       Let ((id, typ), of_syntax def, of_syntax body)
   | Syntax.Var id -> Var id
@@ -36,6 +64,7 @@ let rec to_string exp =
   | Unit -> "()"
   | Int i -> string_of_int i
   | Add (e1, e2) -> Printf.sprintf "(%s + %s)" e1 e2
+  | Sub (e1, e2) -> Printf.sprintf "(%s - %s)" e1 e2
   | Var id -> Id.to_string id
   | Let ((id, _t), e1, e2) ->
       Printf.sprintf "(let %s = %s in %s)" (Id.to_string id) (to_string e1)
