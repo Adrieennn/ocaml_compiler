43b52aeffd9e2894db24f84b7b19349da809cd86
Justus Fasse <justus.fasse@etu.univ-grenoble-alpes.fr>
2020-01-13 19:53:50 +0100

    Fixup alpha.ml

--- ocaml/alpha.ml
+++ ocaml/alpha.ml
@@ -1,51 +1,70 @@
-let find mapping var = 
-	List.assoc_opt var mapping
+let find mapping var = List.assoc_opt var mapping
 
 let replace_name mapping name =
-	match find mapping var with
-	| None -> name
-	| Some(new_name) new_name
-
-
+  match find mapping name with None -> name | Some new_name -> new_name
 
 let rec convert exp mapping =
-	match exp with
-	| Knorm.Unit c | Knorm.Int c |Knorm.Float c -> c
-	| Knorm.Add (v1,v2) -> 
-		let c_v1 = replace_name mapping v1 in 
-		let c_v2 = replace_name mapping v2 in
-		Knorm.Add(c_v1,c_v2)
-	| Knorm.Sub (v1,v2) ->
-		let c_v1 = convert v1 mapping in 
-		let c_v2 = convert v2 mapping in
-		Knorm.Sub(c_v1,c_v2)
-	| Knorm.FAdd (v1,v2) -> 
-		let c_v1 = convert v1 mapping in 
-		let c_v2 = convert v2 mapping in
-		Knorm.FAdd(c_v1,c_v2)
-	| Knorm.FSub (v1,v2) ->
-		let c_v1 = convert v1 mapping in 
-		let c_v2 = convert v2 mapping in
-		Knorm.FSub(c_v1,c_v2)
-	| Knorm.FMul (v1,v2) -> 
-		let c_v1 = convert v1 mapping in 
-		let c_v2 = convert v2 mapping in
-		Knorm.FMul(c_v1,c_v2)
-	| Knorm.FDiv (v1,v2) ->
-		let c_v1 = convert v1 mapping in 
-		let c_v2 = convert v2 mapping in
-		Knorm.FDiv(c_v1,c_v2)
-	| Knorm.Let ((id, typ), def, body) ->
-		let new_mapping = ( match find mapping id with
-		| None -> (id, id) :: mapping
-		| Some(_) -> let suffix = id.genid in (id, id ^ suffix) :: mapping
-		) in
-		Knorm.Let(((id ^ suffix), typ), convert def new_env, convert body new_env)
-	| Knorm.Var x ->
-		(match find mapping x with
-		| None -> Knorm.Var x
-		| Some(new_x) -> Knorm.Var new_x 
-	) 
-	| Knorm.LetRec ((id, typ), args ,body) ->
-	Knorm.LetRec(((replace_name id), typ), List.map (fun (id, t) -> (replace_name mapping id, t)) args, convert body mapping)
-	| _ -> failwith "under construction"
+  match exp with
+  | (Knorm.Unit | Knorm.Int _ | Knorm.Float _) as c -> c
+  | Knorm.Add (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.Add (c_v1, c_v2)
+  | Knorm.Sub (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.Sub (c_v1, c_v2)
+  | Knorm.FAdd (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.FAdd (c_v1, c_v2)
+  | Knorm.FSub (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.FSub (c_v1, c_v2)
+  | Knorm.FMul (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.FMul (c_v1, c_v2)
+  | Knorm.FDiv (v1, v2) ->
+      let c_v1 = replace_name mapping v1 in
+      let c_v2 = replace_name mapping v2 in
+      Knorm.FDiv (c_v1, c_v2)
+  | Knorm.Let ((id, typ), def, body) ->
+      let new_id =
+        match find mapping id with
+        | None -> id
+        | Some _ ->
+            let suffix = Id.genid () in
+            id ^ suffix
+      in
+      Knorm.Let
+        ( (new_id, typ),
+          convert def mapping,
+          convert body ((id, new_id) :: mapping) )
+  | Knorm.Var x -> Knorm.Var (replace_name mapping x)
+  | Knorm.LetRec ({ name = fun_id, fun_typ; args; body = fun_body }, let_body)
+    ->
+      let new_args =
+        List.map (fun (id, t) -> (replace_name mapping id, t)) args
+      in
+      let old_arg_names = List.map (fun (id, _t) -> id) args in
+      let new_arg_names = List.map (fun (id, _t) -> id) new_args in
+      let new_arg_mappings =
+        List.map2
+          (fun old_id new_id -> (old_id, new_id))
+          old_arg_names new_arg_names
+      in
+
+      let new_fun_id = replace_name mapping fun_id in
+      let new_fun_name = (new_fun_id, fun_typ) in
+      Knorm.LetRec
+        ( {
+            name = new_fun_name;
+            args = new_args;
+            body =
+              convert fun_body
+                (((fun_id, new_fun_id) :: new_arg_mappings) @ mapping);
+          },
+          convert let_body ((fun_id, new_fun_id) :: mapping) )
+  | _ -> failwith "under construction"
