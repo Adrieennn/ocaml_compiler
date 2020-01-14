74948eae850292cb1a26af8d83e1425e32154c64
Yidong Huang <yidong.huang@etu.univ-grenoble-alpes.fr>
2020-01-14 14:45:24 +0100

    type def for closure based on paper

---
+++ ocaml/closure.ml
@@ -0,0 +1,35 @@
+type t =
+  | Unit
+  | Int of int
+  | Float of float
+  | Add of Id.t * Id.t
+  | Sub of Id.t * Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t
+  | Let of (Id.t * Type.t) * t * t
+  | Var of Id.t
+  | IfEq of (Id.t * Id.t) * t * t
+  (* cf. BLE branch if less than or equal *)
+  | IfLe of (Id.t * Id.t) * t * t
+  | Tuple of Id.t list
+  | LetTuple of (Id.t * Type.t) list * t * t
+  | Array of Id.t * Id.t
+  | Get of Id.t * Id.t
+  | Put of Id.t * Id.t * Id.t
+  (*| MkCls of (Id.t * Type.t) * (Id.l * (Id.t * Type.t) list) * t*)
+  (*| AppCls of (Id.t * Type.t) * (Id.t * Type.t) list*)
+  | AppDir of (Id.l * Type.t) * (Id.t * Type.t) list
+
+
+type fundef =
+    { name : Id.l * Type.t;
+      args : (Id.t * Type.t) list;
+      formal_fv : (Id.t * Type.t) list;
+      body : t }
+type prog =
+    Prog of fundef list * t
+
+
+
--- ocaml/id.ml
+++ ocaml/id.ml
@@ -1,4 +1,5 @@
 type t = string
+type l = string
 
 let to_string x = x
 
