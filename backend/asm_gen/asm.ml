196b87f273bbbb7cf5ab087ca65464fe377e4169
kessibi <guillaume@kessibi.fr>
2020-01-15 17:13:46 +0100

    backend: generated first if statements labels

--- backend/asm_gen/asm.ml
+++ backend/asm_gen/asm.ml
@@ -1,6 +1,14 @@
 open Asml
 open Register
 
+let label_counter x =
+  let counter = ref x in
+  fun () ->
+    counter := !counter + 1;
+    !counter
+
+let if_count = label_counter 0
+
 (* args_to_asm_pred: NOT spilling args of predefined functions but store them in
  * registers *)
 let rec args_to_asm_pred args regnum =
@@ -22,16 +30,16 @@ let reset_sp args =
   "add r13, r13, #" ^ string_of_int len ^ "\n"
 
 (* exp_to_asm: match exp with corresponding assembly operations *)
-let exp_to_asm exp reg =
+let rec exp_to_asm exp reg =
   match exp with
-  | Add (x, y) ->
-      ( "ldr r4, [r11, #" ^ x ^ "]\n"
+  | Add (x, y) -> (
+      "ldr r4, [r11, #" ^ x ^ "]\n"
       ^
       match y with
       | Int i -> "add r0, r4, #" ^ string_of_int i ^ "\n"
       | Var a -> "ldr r5, [r11, #" ^ a ^ "]\n" ^ "add r0, r4, r5\n" )
-  | Sub (x, y) ->
-      ( "ldr r4, [r11, #" ^ x ^ "]\n"
+  | Sub (x, y) -> (
+      "ldr r4, [r11, #" ^ x ^ "]\n"
       ^
       match y with
       | Int i -> "sub r0, r4, #" ^ string_of_int i ^ "\n"
@@ -41,24 +49,29 @@ let exp_to_asm exp reg =
         String.length label > 9
         && String.equal (String.sub label 0 9) "_min_caml"
       then
-        args_to_asm_pred args 0 ^ "bl "
-        ^ Id.remove_label_undersc label
-        ^ "\n"
+        args_to_asm_pred args 0 ^ "bl " ^ Id.remove_label_undersc label ^ "\n"
       else
         args_to_asm args ^ "mov r11, r13 @ move sp to fp\n" ^ "bl "
         ^ Id.remove_label_undersc label
         ^ "\n" ^ "mov r13, r11 @ move fp to sp\n" ^ "ldr r11, [r11]\n"
         ^ reset_sp args
   | IfEq (s1, s2, t1, t2) ->
-    "ldr r4, [r11, #" ^ s1 ^ "]\n"
-    ^
-    (match s2 with
-    | Int i -> "mov r5, #" ^ string_of_int i ^ "\n"
-    | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n")
+      let label_index = string_of_int (if_count ()) in
+      ( "ldr r4, [r11, #" ^ s1 ^ "]\n"
+      ^
+      match s2 with
+      | Int i -> "mov r5, #" ^ string_of_int i ^ "\n"
+      | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n" )
+      ^ "cmp r4, r5\n beq ltrue" ^ label_index ^ "\n" ^
+      "b lfalse" ^ label_index ^ "\n" ^
+      "ltrue" ^ label_index ^":\n" ^
+      t_to_asm t1 reg ^ "b lnext" ^ label_index ^ "\n" ^
+      "lfalse" ^ label_index ^ ":\n" ^ t_to_asm t2 reg ^ 
+      "lnext" ^ label_index ^ ":\n"
   | _ -> "@ IGNORED FOR NOW"
 
 (* t_to_asm: transform let and exp to assembly *)
-let rec t_to_asm body reg =
+and t_to_asm body reg =
   match body with
   | Let ((id, _), e, t) ->
       ( match e with
