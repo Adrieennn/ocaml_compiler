bc1455d4ef019c788e6f35ebb94739830538bbb5
kessibi <guillaume@kessibi.fr>
2020-01-16 17:18:28 +0100

    Makefile: back to testing with tools/asml

--- tests/asml_gen/suite.sh
+++ tests/asml_gen/suite.sh
@@ -6,7 +6,7 @@ echo -e "--> Simple arithmetics\n"
 
 for f in tests/asml_gen/arithm/*; do
     printf "$(basename "$f"):\t"
-    ./$PROG $f > tmp.asml
+    ./$PROG $ASML $f > tmp.asml
     ./tools/asml tmp.asml &> /dev/null && echo "OK" || exit 1
 done
 
