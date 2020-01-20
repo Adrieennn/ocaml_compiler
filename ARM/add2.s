26fc6b13b7240ef1012e9e3631a9a8329e01060e
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2020-01-20 16:25:57 +0100

    backend: modified add2.s to test create array

--- ARM/add2.s
+++ ARM/add2.s
@@ -1,14 +1,54 @@
+.text
   .global _start
-_start:
-  @ move sp to fp
-  mov r11, r13
-  mov r4, #3
-  push {r4}
-  mov r4, #5
-  push {r4}
-  ldr r4, [r11, #-4]
-  ldr r5, [r11, #-8]
-  add r4, r4, r5
-  mov r0, r4 @ print_int reads in r0
-  bl min_caml_print_int
 
+_start:
+mov r11, r13
+bl min_caml_mmap
+
+@ str r1, [r12, #0]
+@ str r1, [r12, #8]
+@ mov r0, r12
+@ bl min_caml_print_int
+@ bl min_caml_print_newline
+@ ldr r0, [r12, #0]
+@ bl min_caml_print_int
+@ bl min_caml_print_newline
+@ ldr r0, [r12, #4]
+@ bl min_caml_print_int
+@ bl min_caml_print_newline
+@ ldr r0, [r12, #8]
+@ bl min_caml_print_int
+@ bl min_caml_print_newline
+@ ldr r0, [r12, #12]
+@ bl min_caml_print_int
+@ bl min_caml_print_newline
+
+mov r0, r12
+bl min_caml_print_int
+bl min_caml_print_newline
+
+mov r0, #3
+mov r1, #42
+
+bl min_caml_create_array
+push {r0}
+
+ldr r0, [r11, #-4]
+ldr r0, [r0]
+bl min_caml_print_int
+bl min_caml_print_newline
+
+ldr r0, [r11, #-4]
+ldr r0, [r0, #4]
+bl min_caml_print_int
+bl min_caml_print_newline
+
+ldr r0, [r11, #-4]
+ldr r0, [r0, #8]
+bl min_caml_print_int
+bl min_caml_print_newline
+
+ldr r0, [r11, #-4]
+ldr r0, [r0, #12]
+bl min_caml_print_int
+bl min_caml_print_newline
