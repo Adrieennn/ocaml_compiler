17c8db409def3e222757e495b1b593ae206b3879
Justus Fasse <justus.fasse@etu.univ-grenoble-alpes.fr>
2020-01-20 14:50:52 +0100

    Frontend (Beta): ocamlformat

--- frontend/beta.ml
+++ frontend/beta.ml
@@ -15,7 +15,6 @@ variable name using Id.genid when encountering a non-external variable.
 let new_name mapping name =
   match find mapping name with None -> name | Some _ -> name ^ Id.genid ()
 
-
 let rec convert exp mapping =
   match exp with
   | (Knorm.Unit | Knorm.Int _ | Knorm.Float _) as c -> c
@@ -44,53 +43,46 @@ let rec convert exp mapping =
       let c_v2 = replace_name mapping v2 in
       Knorm.FDiv (c_v1, c_v2)
   | Knorm.Var x -> Knorm.Var (replace_name mapping x)
-
-  | Knorm.Let ((id, typ), def, body) ->
+  | Knorm.Let ((id, typ), def, body) -> (
       (*In the case of a Let expression, we use the updated mapping environment 
           to evaluate the let body*)
-    let new_def = convert def mapping in 
-    (match new_def with
-    | Knorm.Var new_id -> convert body ((id, new_id) :: mapping)
-    | _ -> Knorm.Let((id, typ), new_def, convert body mapping))
-
-    (*only convert fun_body and let_body*)
-  | Knorm.LetRec ({ name = fun_id, fun_typ; args; body = fun_body }, let_body) -> 
-      Knorm.LetRec ({ name = fun_id, fun_typ; args; body = (convert fun_body mapping)}, convert let_body mapping)
-    (*only remove t*)
+      let new_def = convert def mapping in
+      match new_def with
+      | Knorm.Var new_id -> convert body ((id, new_id) :: mapping)
+      | _ -> Knorm.Let ((id, typ), new_def, convert body mapping) )
+  (*only convert fun_body and let_body*)
+  | Knorm.LetRec ({ name = fun_id, fun_typ; args; body = fun_body }, let_body)
+    ->
+      Knorm.LetRec
+        ( { name = (fun_id, fun_typ); args; body = convert fun_body mapping },
+          convert let_body mapping )
+  (*only remove t*)
   | Knorm.LetTuple (vars, def, body) ->
-  		Knorm.LetTuple(vars,convert def mapping,convert body mapping)
-      (*match vars with
-      | [] -> expr
-      | _ -> expr2*)
-   (*failwith "beta conversion for LetTuple is not implemented yet"*)
-
+      (* TODO?: Beta reduction on LetTuples *)
+      Knorm.LetTuple (vars, convert def mapping, convert body mapping)
   | Knorm.App (f, args) ->
       let new_args = List.map (fun id -> replace_name mapping id) args in
       Knorm.App (f, new_args)
-
   | Knorm.IfEq ((v1, v2), e1, e2) ->
       let c_v1 = replace_name mapping v1 in
       let c_v2 = replace_name mapping v2 in
       Knorm.IfEq ((c_v1, c_v2), convert e1 mapping, convert e2 mapping)
-
   | Knorm.IfLe ((v1, v2), e1, e2) ->
       let c_v1 = replace_name mapping v1 in
       let c_v2 = replace_name mapping v2 in
       Knorm.IfLe ((c_v1, c_v2), convert e1 mapping, convert e2 mapping)
-
-  | Knorm.Tuple(tups) ->
-      let new_tups = List.map (fun tup -> (replace_name mapping tup)) tups in
-      Knorm.Tuple(new_tups)
-
-  | Knorm.Array (v1,v2) ->
+  | Knorm.Tuple tups ->
+      let new_tups = List.map (fun tup -> replace_name mapping tup) tups in
+      Knorm.Tuple new_tups
+  | Knorm.Array (v1, v2) ->
       let c_v1 = replace_name mapping v1 in
       let c_v2 = replace_name mapping v2 in
       Knorm.Array (c_v1, c_v2)
-  | Knorm.Get (v1,v2) ->
+  | Knorm.Get (v1, v2) ->
       let c_v1 = replace_name mapping v1 in
       let c_v2 = replace_name mapping v2 in
       Knorm.Get (c_v1, c_v2)
-  | Knorm.Put (v1,v2,v3) ->
+  | Knorm.Put (v1, v2, v3) ->
       let c_v1 = replace_name mapping v1 in
       let c_v2 = replace_name mapping v2 in
       let c_v3 = replace_name mapping v3 in
