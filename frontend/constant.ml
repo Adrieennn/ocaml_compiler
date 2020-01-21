843af6f66a031736c40c8040ffef62fa3ee0f204
Fatima Ali <fatima-zahrau.ali@grenoble-inp.org>
2020-01-21 16:39:51 +0100

    Complete constant folding

--- frontend/constant.ml
+++ frontend/constant.ml
@@ -3,45 +3,94 @@
 (*Finds the associated definition of var in mapping. If *)
 let in_mapping mapping var =
   match (List.assoc_opt var mapping) with
-  | None -> Knorm.Unit (*is it better to write "false"?*)
+  | None -> var (*failwith "To do - failed search" *)
   | Some def -> def
 
-(*If a variable definition is a constant (int/float), replace the variable with the constant*)
+(*If a variable definition is a constant (int/float/tuple), replace the variable with the constant*)
 let replace_with_constant mapping var =
   let definition  = find mapping var in
   match definition with
   | Knorm.Int i -> i
   | Knorm.Float d -> d
   | Knorm.Tuple tups -> tups
-  | _ -> false
+  | _ -> var
+
+(*Checks if a variable definition is an integer constant*)
+let is_int v =
+    match v with
+    | Knorm.Int i -> true | _ -> false
+
+(*Checks if a variable definition is a float constant*)
+let is_float v =
+    match v with
+    | Knorm.Float d -> true | _ -> false
+
+(*Checks if a variable definition is a tuple constant*)
+let is_tuple v =
+    match v with
+    | Knorm.Tuple tups -> true | _ -> false
 
 (*Constant folding on Knorm.t. It takes an expression and mapping, and returns a constant folded expression *)
 let rec folding exp mapping =
   match exp with
-  | (Knorm.Unit | Knorm.Int _ | Knorm.Float _) as c -> c
+  | (Knorm.Unit | Knorm.Int _ | Knorm.Float _ | Knorm.App _ | Knorm.Tuple _| Knorm.Array _ | Knorm.Get _ | Knorm.Put _) as c -> c
   | Knorm.Add (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in   (*Need to enforce constant is of type int*)
-    let c_v2 = replace_with_constant mapping v2 in
-    Knorm.Add (c_v1, c_v2)
-    if c_v1 && c_v2  then Knorm.Int (c_v1 + c_v2)
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_int c_v1) && (is_int c_v2)) then Knorm.Int (c_v1 + c_v2)
+      else Knorm.Add(c_v1, c_v2)
   | Knorm.Sub (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in
-    let c_v2 = replace_with_constant mapping v2 in
-    Knorm.Sub (c_v1, c_v2)
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_int c_v1) && (is_int c_v2)) then Knorm.Int (c_v1 - c_v2)
+      else Knorm.Sub (c_v1, c_v2)
   | Knorm.FAdd (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in
-    let c_v2 = replace_with_constant mapping v2 in  (*Need to enforce constant is of type float?*)
-    Knorm.FAdd (c_v1, c_v2)
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_float c_v1) && (is_float c_v2)) then Knorm.Float (c_v1 +. c_v2)
+      else Knorm.FAdd (c_v1, c_v2)
   | Knorm.FSub (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in
-    let c_v2 = replace_with_constant mapping v2 in
-    Knorm.FSub (c_v1, c_v2)
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_float c_v1) && (is_float c_v2)) then Knorm.Float (c_v1 -. c_v2)
+      else Knorm.FSub (c_v1, c_v2)
   | Knorm.FMul (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in
-    let c_v2 = replace_with_constant mapping v2 in
-    Knorm.FMul (c_v1, c_v2)
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_float c_v1) && (is_float c_v2)) then Knorm.Float (c_v1 *. c_v2)
+      else Knorm.FMul (c_v1, c_v2)
   | Knorm.FDiv (v1, v2) ->
-    let c_v1 = replace_with_constant mapping v1 in
-    let c_v2 = replace_with_constant mapping v2 in
-    Knorm.FDiv (c_v1, c_v2)
-  | _ -> failwith "Not implemented"
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      if ((is_float c_v1) && (is_float c_v2)) then Knorm.Float (c_v1 /. c_v2)
+      else Knorm.FDiv (c_v1, c_v2)
+  | Knorm.Let ((id, typ), def, body) ->
+      let new_def = folding def mapping in
+      let new_mapping = (id, new_def) :: mapping in
+      Knorm.Let ((id, typ), new_def, folding body new_mapping)
+  | Knorm.Var x ->
+      let c_x = replace_with_constant mapping x in
+      if is_int c_x then Knorm.Int c_x
+      else if is_float c_x then Knorm.Float c_x
+      else if is_tuple c_x then Knorm.Tuple c_x
+      else Knorm.Var c_x
+  | Knorm.LetRec ({ name = fun_id, fun_Ttyp; args; body = fun_body }, let_body) ->
+      let new_fun_body = folding fun_body mapping in
+      let new_let_body = folding let_body mapping in
+      Knorm.LetRec ({name; args; body = new_fun_body}, let_body = new_let_body)
+  | Knorm.IfEq ((v1, v2), e1, e2) ->
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      Knorm.IfEq ((c_v1, c_v2), folding e1 mapping, folding e2 mapping)
+  | Knorm.IfLe ((v1, v2), e1, e2) ->
+      let c_v1 = replace_with_constant mapping v1 in
+      let c_v2 = replace_with_constant mapping v2 in
+      Knorm.IfLe ((c_v1, c_v2), folding e1 mapping, folding e2 mapping)
+  | Knorm.LetTuple (vars, def, body) ->
+      (*if is_tuple new_def then
+        failwith "To Do"
+        let new_def = folding def mapping in
+        let new_mapping =
+        Knorm.LetTuple (vars, def = new_def, folding body new_mapping)
+        else *)
+      Knorm.LetTuple (vars, def, folding def body)
