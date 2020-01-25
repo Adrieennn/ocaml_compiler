aefd036f9a21a948f764573b461c3e81c9b186aa
Justus Fasse <justus.fasse@etu.univ-grenoble-alpes.fr>
2020-01-26 00:49:38 +0100

    Frontend: Refactorization of elim.ml
    Makes matmul.ml work

--- frontend/elim.ml
+++ frontend/elim.ml
@@ -1,39 +1,48 @@
-(**
-effect takes 2 parameters, an expression and a mutable list.
-This function checks if the given expression has a side effect or not by 
-comparing the function names used in the expression to a list of function 
-that's know to have side effects.
- *)
-let rec effect e (l : 'a list ref) =
+module StringSet = Set.Make (String)
+
+let effectful_functions = ref StringSet.empty
+
+(* effect takes 2 parameters, an expression and a mutable list.
+ * This function checks if the given expression has a side effect or not by
+ * comparing the function names used in the expression to a list of function
+ * that's know to have side effects. *)
+let rec effect e =
   match e with
-  | Knorm.Unit | Knorm.Int _ | Knorm.Float _ | Knorm.Tuple _ | Knorm.Array _
-  | Knorm.Get _ | Knorm.Var _ ->
-      false
-  | Knorm.Add _ | Knorm.Sub _ | Knorm.FAdd _ | Knorm.FSub _ | Knorm.FMul _
-  | Knorm.FDiv _ ->
-      false
-  | Knorm.App (id, args) -> (
-      match List.find_opt (fun elm -> elm = id) !l with
-      | Some _ -> true
-      | None -> false )
-  | Knorm.Let (var, def, body) -> effect def l && effect body l
+  | Knorm.Put _ -> true
+  | Knorm.App (id, args) -> StringSet.mem id !effectful_functions
   | Knorm.LetRec ({ name = fun_id, fun_typ; args; body = fun_body }, let_body)
     ->
-      if effect fun_body l then (
-        l := fun_id :: !l;
-        true )
-      else effect let_body l
-  | Knorm.LetTuple (vars, def, body) -> effect def l && effect body l
-  | Knorm.Put _ -> true
-  | Knorm.IfEq ((v1, v2), e1, e2) -> effect e1 l && effect e2 l
-  | Knorm.IfLe ((v1, v2), e1, e2) -> effect e1 l && effect e2 l
+      (* We must run `effect` on fun_body so that
+       * the mutable list of effectful functions is updated. That is when checking
+       * the `let_body`, we must detect those functions which were defined inside of
+       * the current function definition too, including the function being currently
+       * defined itself *)
+      if effect fun_body then
+        effectful_functions := StringSet.add fun_id !effectful_functions;
 
-(**
-fvar takes 2 paremeters, a variable name and an expression.
-This function checks if the variable is a free variable in the given 
-expression.
-*)
-let rec fvar (id : Id.t) (exp : Knorm.t) =
+      (* Even if a fun_body is effectful, the LetRec might not be if that function
+       * is not actaully run inside the body. *)
+      effect let_body
+  | Knorm.Let (var, def, body) ->
+      let b1 = effect def in
+      let b2 = effect body in
+      b1 || b2
+  | Knorm.LetTuple (vars, def, body) ->
+      let b1 = effect def in
+      let b2 = effect body in
+      b1 || b2
+  | Knorm.IfEq ((v1, v2), e1, e2) | Knorm.IfLe ((v1, v2), e1, e2) ->
+      let b1 = effect e1 in
+      let b2 = effect e2 in
+      b1 || b2
+  | Knorm.Unit | Knorm.Int _ | Knorm.Float _ | Knorm.Tuple _ | Knorm.Array _
+  | Knorm.Get _ | Knorm.Var _ | Knorm.Add _ | Knorm.Sub _ | Knorm.FAdd _
+  | Knorm.FSub _ | Knorm.FMul _ | Knorm.FDiv _ ->
+      false
+
+(* not_occurs takes 2 paremeters, a variable name and an expression. This
+ * function checks if the variable does not occur in the given expression. *)
+let rec not_occurs (id : Id.t) (exp : Knorm.t) =
   match exp with
   | Knorm.Unit | Knorm.Int _ | Knorm.Float _ -> true
   | Knorm.Put (e1, e2, e3) -> id <> e1 && id <> e2 && id <> e3
@@ -49,41 +58,48 @@ let rec fvar (id : Id.t) (exp : Knorm.t) =
   | Knorm.Tuple l -> List.for_all (fun id' -> id <> id') l
   | Knorm.Get (e1, e2) -> id <> e1 && id <> e2
   (* assume that after alpha conversion all variable name is different *)
-  | Knorm.Let ((name, _t), e1, e2) -> fvar id e1 && fvar id e2
+  | Knorm.Let ((name, _t), e1, e2) -> not_occurs id e1 && not_occurs id e2
   | Knorm.LetRec ({ name; args; body }, let_body) ->
-      fvar id body && fvar id let_body
-  | Knorm.LetTuple (l, e1, e2) -> fvar id e1 && fvar id e2
+      not_occurs id body && not_occurs id let_body
+  | Knorm.LetTuple (l, e1, e2) -> not_occurs id e1 && not_occurs id e2
   | Knorm.IfEq ((v1, v2), e1, e2) | Knorm.IfLe ((v1, v2), e1, e2) ->
-      id <> v1 && id <> v2 && fvar id e1 && fvar id e2
+      id <> v1 && id <> v2 && not_occurs id e1 && not_occurs id e2
   | Knorm.App (f, args) -> f <> id && List.for_all (fun id' -> id <> id') args
 
-(** 
-elim takes 2 parameters, an expression and a list 
-This function is the main body of elimination, it checks every definition and 
-determine if it's unnecessary. If so the definition will be suppressed.
-*)
-let rec elim exp l =
+(* elim takes 2 parameters, an expression and a list. This function is the main body
+ * of elimination, it checks every definition and determine if it's unnecessary. If so
+ * the definition will be suppressed. *)
+let rec elim exp =
   match exp with
   | Knorm.Let ((id, _t), e1, e2) ->
-      if (not (effect e1 l)) && fvar id e2 then elim e2 l
-      else Knorm.Let ((id, _t), elim e1 l, elim e2 l)
+      if (not (effect e1)) && not_occurs id e2 then elim e2
+      else Knorm.Let ((id, _t), elim e1, elim e2)
   | Knorm.LetRec
       ({ Knorm.name = fun_id, fun_typ; args; body = fun_body }, let_body) ->
-      if (not (effect fun_body l)) && fvar fun_id let_body then elim let_body l
-      else
+      if (not (effect fun_body)) && not_occurs fun_id let_body then
+        elim let_body
+      else (
+        (* It is necessary to add the function here to because we will otherwise miss
+         * an effectful occurence of the outermost function since it will not be added
+         * to the list of effectful functions by `not_occurs`. *)
+        effectful_functions := StringSet.add fun_id !effectful_functions;
         Knorm.LetRec
-          ( { name = (fun_id, fun_typ); args; body = elim fun_body l },
-            elim let_body l )
+          ( { name = (fun_id, fun_typ); args; body = elim fun_body },
+            elim let_body ) )
   | Knorm.LetTuple (vars, def, body) ->
-      Knorm.LetTuple (vars, elim def l, elim body l)
-  | Knorm.IfEq ((v1, v2), e1, e2) -> Knorm.IfEq ((v1, v2), elim e1 l, elim e2 l)
-  | Knorm.IfLe ((v1, v2), e1, e2) -> Knorm.IfLe ((v1, v2), elim e1 l, elim e2 l)
-  | _ as c -> c
+      Knorm.LetTuple (vars, elim def, elim body)
+  | Knorm.IfEq ((v1, v2), e1, e2) -> Knorm.IfEq ((v1, v2), elim e1, elim e2)
+  | Knorm.IfLe ((v1, v2), e1, e2) -> Knorm.IfLe ((v1, v2), elim e1, elim e2)
+  | ( Knorm.Unit | Knorm.Int _ | Knorm.Float _ | Knorm.Tuple _ | Knorm.Array _
+    | Knorm.Get _ | Knorm.Var _ | Knorm.Add _ | Knorm.Sub _ | Knorm.FAdd _
+    | Knorm.FSub _ | Knorm.FMul _ | Knorm.FDiv _ | Knorm.App _ | Knorm.Put _ )
+    as non_definition -> non_definition
 
-(**
-elimination takes an expression as a parameter.
-This function uses elim defined previously to eliminate unnecessary definitions. 
-*)
+(* elimination takes an expression as a parameter.
+ * This function uses elim defined previously to eliminate unnecessary definitions. *)
 let elimination exp =
-  let l = ref [] in
-  elim exp l
+  (* reset mutable state *)
+  effectful_functions :=
+    StringSet.empty |> StringSet.add "print_int"
+    |> StringSet.add "print_newline";
+  elim exp
