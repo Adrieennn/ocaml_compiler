7f7a667341cf399c1bbf6172d17c7edd90832bcb
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2020-01-27 16:07:25 +0100

    Backend: Modified registers r11, r14 and r15 to their aliases fp, lr and pc for easier reading.

--- backend/asm_gen/asm.ml
+++ backend/asm_gen/asm.ml
@@ -24,21 +24,21 @@ let move_integer register i =
 let rec args_to_asm_pred args regnum =
   match args with
   | h :: r ->
-      "ldr r" ^ string_of_int regnum ^ ", [r11, #" ^ h ^ "]\n"
+      "ldr r" ^ string_of_int regnum ^ ", [fp, #" ^ h ^ "]\n"
       ^ args_to_asm_pred r (regnum + 1)
   | [] -> ""
 
 (* args_to_asm: spilling arguments of user-defined functions *)
 let rec args_to_asm args =
   match args with
-  | h :: r -> "ldr r0, [r11, #" ^ h ^ "]\n" ^ "push {r0}\n" ^ args_to_asm r
-  | [] -> "add sp, sp, #-8 @ making space for lr and %self\n" ^ "push {r11}\n"
+  | h :: r -> "ldr r0, [fp, #" ^ h ^ "]\n" ^ "push {r0}\n" ^ args_to_asm r
+  | [] -> "add sp, sp, #-8 @ making space for lr and %self\n" ^ "push {fp}\n"
 
 let rec args_to_asm_closure args =
   match args with
   | h :: r ->
-      "ldr r0, [r11, #" ^ h ^ "]\n" ^ "push {r0}\n" ^ args_to_asm_closure r
-  | [] -> "add sp, sp, #-4 @ making space for lr\n" ^ "push {r11}\n"
+      "ldr r0, [fp, #" ^ h ^ "]\n" ^ "push {r0}\n" ^ args_to_asm_closure r
+  | [] -> "add sp, sp, #-4 @ making space for lr\n" ^ "push {fp}\n"
 
 (* reset_sp: move stack pointer back to before function call *)
 let reset_sp args =
@@ -51,18 +51,18 @@ let rec exp_to_asm exp =
   | Unit -> ""
   | Int i -> move_integer "r0" i
   | Label l -> "ldr r0, =" ^ Id.remove_label_undersc l ^ "\n"
-  | Var v -> "ldr r0, [r11, #" ^ v ^ "]\n"
+  | Var v -> "ldr r0, [fp, #" ^ v ^ "]\n"
   | Add (x, y) ->
-      "ldr r4, [r11, #" ^ x ^ "]\n"
+      "ldr r4, [fp, #" ^ x ^ "]\n"
       ^ ( match y with
         | Int i -> move_integer "r5" i
-        | Var a -> "ldr r5, [r11, #" ^ a ^ "]\n" )
+        | Var a -> "ldr r5, [fp, #" ^ a ^ "]\n" )
       ^ "add r0, r4, r5\n"
   | Sub (x, y) ->
-      "ldr r4, [r11, #" ^ x ^ "]\n"
+      "ldr r4, [fp, #" ^ x ^ "]\n"
       ^ ( match y with
         | Int i -> move_integer "r5" i
-        | Var a -> "ldr r5, [r11, #" ^ a ^ "]\n" )
+        | Var a -> "ldr r5, [fp, #" ^ a ^ "]\n" )
       ^ "sub r0, r4, r5\n"
   | CallDir (label, args) ->
       if
@@ -71,18 +71,18 @@ let rec exp_to_asm exp =
       then
         args_to_asm_pred args 0 ^ "bl " ^ Id.remove_label_undersc label ^ "\n"
       else
-        args_to_asm args ^ "mov r11, sp @ move sp to fp\n" ^ "bl "
+        args_to_asm args ^ "mov fp, sp @ move sp to fp\n" ^ "bl "
         ^ Id.remove_label_undersc label
-        ^ "\n" ^ "mov sp, r11 @ move fp to sp\n" ^ "ldr r11, [r11]\n"
+        ^ "\n" ^ "mov sp, fp @ move fp to sp\n" ^ "ldr fp, [fp]\n"
         ^ reset_sp args
   | CallCls (label, args) ->
       args_to_asm_closure (args @ [ label ])
-      ^ "mov r11, sp @ move sp to fp\n" ^ "ldr r0, [r11, #8]\n"
-      ^ "ldr r0, [r0]\n" ^ "blx r0\n" ^ "mov sp, r11 @ move fp to sp\n"
-      ^ "ldr r11, [r11]\n" ^ reset_sp args
+      ^ "mov fp, sp @ move sp to fp\n" ^ "ldr r0, [fp, #8]\n"
+      ^ "ldr r0, [r0]\n" ^ "blx r0\n" ^ "mov sp, fp @ move fp to sp\n"
+      ^ "ldr fp, [fp]\n" ^ reset_sp args
   | IfFEq (s1, s2, t1, t2) ->
       let label_index = string_of_int (if_count ()) in
-      ("ldr r4, [r11, #" ^ s1 ^ "]\n" ^ "ldr r5, [r11, #" ^ s2 ^ "]\n")
+      ("ldr r4, [fp, #" ^ s1 ^ "]\n" ^ "ldr r5, [fp, #" ^ s2 ^ "]\n")
       ^ "vmov.f32 s0, r4\n" ^ "vmov.f32 s1, r5\n" ^ "vcmp.f32 s0, s1\n"
       ^ "vmrs     APSR_nzcv, FPSCR    @ Get the flags into APSR.\n"
       ^ "beq ltrue" ^ label_index ^ "\n" ^ "b lfalse" ^ label_index ^ "\n"
@@ -91,7 +91,7 @@ let rec exp_to_asm exp =
       ^ label_index ^ ":\n"
   | IfFLEq (s1, s2, t1, t2) ->
       let label_index = string_of_int (if_count ()) in
-      ("ldr r4, [r11, #" ^ s1 ^ "]\n" ^ "ldr r5, [r11, #" ^ s2 ^ "]\n")
+      ("ldr r4, [fp, #" ^ s1 ^ "]\n" ^ "ldr r5, [fp, #" ^ s2 ^ "]\n")
       ^ "vmov.f32 s0, r4\n" ^ "vmov.f32 s1, r5\n" ^ "vcmp.f32 s0, s1\n"
       ^ "vmrs     APSR_nzcv, FPSCR    @ Get the flags into APSR.\n"
       ^ "ble ltrue" ^ label_index ^ "\n" ^ "b lfalse" ^ label_index ^ "\n"
@@ -100,50 +100,50 @@ let rec exp_to_asm exp =
       ^ label_index ^ ":\n"
   | IfEq (s1, s2, t1, t2) ->
       let label_index = string_of_int (if_count ()) in
-      ( "ldr r4, [r11, #" ^ s1 ^ "]\n"
+      ( "ldr r4, [fp, #" ^ s1 ^ "]\n"
       ^
       match s2 with
       | Int i -> move_integer "r5" i
-      | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n" )
+      | Var v -> "ldr r5, [fp, #" ^ v ^ "]\n" )
       ^ "cmp r4, r5\n" ^ "beq ltrue" ^ label_index ^ "\n" ^ "b lfalse"
       ^ label_index ^ "\n" ^ "ltrue" ^ label_index ^ ":\n" ^ t_to_asm t1 0
       ^ "b lnext" ^ label_index ^ "\n" ^ "lfalse" ^ label_index ^ ":\n"
       ^ t_to_asm t2 0 ^ "lnext" ^ label_index ^ ":\n"
   | IfLEq (s1, s2, t1, t2) ->
       let label_index = string_of_int (if_count ()) in
-      ( "ldr r4, [r11, #" ^ s1 ^ "]\n"
+      ( "ldr r4, [fp, #" ^ s1 ^ "]\n"
       ^
       match s2 with
       | Int i -> move_integer "r5" i
-      | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n" )
+      | Var v -> "ldr r5, [fp, #" ^ v ^ "]\n" )
       ^ "cmp r4, r5\n" ^ "ble ltrue" ^ label_index ^ "\n" ^ "b lfalse"
       ^ label_index ^ "\n" ^ "ltrue" ^ label_index ^ ":\n" ^ t_to_asm t1 0
       ^ "b lnext" ^ label_index ^ "\n" ^ "lfalse" ^ label_index ^ ":\n"
       ^ t_to_asm t2 0 ^ "lnext" ^ label_index ^ ":\n"
   | Ld (s1, s2) ->
-      "ldr r4, [r11, #" ^ s1 ^ "]\n"
+      "ldr r4, [fp, #" ^ s1 ^ "]\n"
       ^ ( match s2 with
-        | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n"
+        | Var v -> "ldr r5, [fp, #" ^ v ^ "]\n"
         | Int i -> move_integer "r5" i )
       ^ "lsl r5, r5, #2\nldr r0, [r4, r5]\n"
   | St (s1, s2, s3) ->
-      "ldr r4, [r11, #" ^ s1 ^ "]\n"
+      "ldr r4, [fp, #" ^ s1 ^ "]\n"
       ^ ( match s2 with
-        | Var v -> "ldr r5, [r11, #" ^ v ^ "]\n"
+        | Var v -> "ldr r5, [fp, #" ^ v ^ "]\n"
         | Int i -> move_integer "r5" i )
-      ^ "ldr r6, [r11, #" ^ s3 ^ "]\n" ^ "lsl r5, r5, #2\nstr r6, [r4, r5]\n"
+      ^ "ldr r6, [fp, #" ^ s3 ^ "]\n" ^ "lsl r5, r5, #2\nstr r6, [r4, r5]\n"
   | New i -> "mov r0, r12\n" ^ move_integer "r4" i ^ "add r12, r12, r4\n"
   | FAdd (s1, s2) ->
-      "vldr s0, [r11, #" ^ s1 ^ "]\n" ^ "vldr s1, [r11, #" ^ s2 ^ "]\n"
+      "vldr s0, [fp, #" ^ s1 ^ "]\n" ^ "vldr s1, [fp, #" ^ s2 ^ "]\n"
       ^ "vadd.f32 s0, s0, s1\n" ^ "vmov r0, s0\n"
   | FSub (s1, s2) ->
-      "vldr s0, [r11, #" ^ s1 ^ "]\n" ^ "vldr s1, [r11, #" ^ s2 ^ "]\n"
+      "vldr s0, [fp, #" ^ s1 ^ "]\n" ^ "vldr s1, [fp, #" ^ s2 ^ "]\n"
       ^ "vsub.f32 s0, s0, s1\n" ^ "vmov r0, s0\n"
   | FMul (s1, s2) ->
-      "vldr s0, [r11, #" ^ s1 ^ "]\n" ^ "vldr s1, [r11, #" ^ s2 ^ "]\n"
+      "vldr s0, [fp, #" ^ s1 ^ "]\n" ^ "vldr s1, [fp, #" ^ s2 ^ "]\n"
       ^ "vmul.f32 s0, s0, s1\n" ^ "vmov r0, s0\n"
   | FDiv (s1, s2) ->
-      "vldr s0, [r11, #" ^ s1 ^ "]\n" ^ "vldr s1, [r11, #" ^ s2 ^ "]\n"
+      "vldr s0, [fp, #" ^ s1 ^ "]\n" ^ "vldr s1, [fp, #" ^ s2 ^ "]\n"
       ^ "vdiv.f32 s0, s0, s1\n" ^ "vmov r0, s0\n"
   | e -> Printf.sprintf "%s IGNORED FOR NOW\n" (Asml.to_string e)
 
@@ -153,7 +153,7 @@ and t_to_asm body sp_reset =
   | Let ((id, _), e, t) ->
       ( match e with
       | Int i -> move_integer "r0" i
-      | Var a -> "ldr r0, [r11, #" ^ a ^ "]\n"
+      | Var a -> "ldr r0, [fp, #" ^ a ^ "]\n"
       | _ -> exp_to_asm e )
       ^ "push {r0}\n"
       ^ t_to_asm t (sp_reset + 4)
@@ -173,8 +173,8 @@ let rec lfu_to_asm lfu =
       ^ Id.remove_label_undersc fu.name
       ^ "\n"
       ^ Id.remove_label_undersc fu.name
-      ^ ":\n" ^ "str r14, [r11, #4] @ store lr on the stack\n"
-      ^ t_to_asm fu.body 0 ^ "ldr r15, [r11, #4] @ load lr (fp + 4) into pc\n"
+      ^ ":\n" ^ "str lr, [fp, #4] @ store lr on the stack\n"
+      ^ t_to_asm fu.body 0 ^ "ldr pc, [fp, #4] @ load lr (fp + 4) into pc\n"
       ^ lfu_to_asm r
   | [] -> "\n"
 
@@ -194,7 +194,7 @@ let prog_to_asm prog =
       ^ {|  .global _start
 
 _start:
-mov r11, sp @ move sp to fp
+mov fp, sp @ move sp to fp
 bl min_caml_mmap
 |}
       ^ t_to_asm body 0 ^ "\n"
